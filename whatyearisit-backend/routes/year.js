1d8a4745767b096ef10f9c1e2eaf1e7a29c3107f
Your name <Your email>
2024-09-20 16:39:02 +0200

    que tu veut

--- whatyearisit-backend/routes/year.js
+++ whatyearisit-backend/routes/year.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
- const date = new Date().getFullYear();
+ const date = new Date().getFullYear().toString();
  res.json({ year: date });
 });
 
